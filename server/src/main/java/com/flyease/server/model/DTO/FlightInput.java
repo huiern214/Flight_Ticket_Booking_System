f60585b82ef3c626bb0505ebbf0f68beabb05d6f
Mushroom <iewgnimeel@gmail.com>
2024-01-10 20:56:24 +0800

    Add additional properties

--- server/src/main/java/com/flyease/server/model/DTO/FlightInput.java
+++ server/src/main/java/com/flyease/server/model/DTO/FlightInput.java
@@ -1,47 +1,153 @@
 package com.flyease.server.model.DTO;
 
 public class FlightInput {
-    private String flightDate;
-    private String flightTime;
+    private String flightDepartureDate;
+    private String flightDepartureTime;
+    private String flightArrivalDate;
+    private String flightArrivalTime;
     private double flightPrice;
     private int flightTotalSeats;
     private int flightTotalPassengers;
+    private double flightBusinessPrice;
+    private int flightTotalBusinessSeats; //newly created
+    private int flightTotalBusinessPassengers;
 
-    public FlightInput(String flightDate, String flightTime, double flightPrice, int flightTotalSeats) {
-        this.flightDate = flightDate;
-        this.flightTime = flightTime;
+    
+
+    public FlightInput(String flightDepartureDate, String flightDepartureTime, String flightArrivalDate,
+            String flightArrivalTime, double flightPrice, int flightTotalSeats, int flightTotalPassengers,
+            double flightBusinessPrice, int flightTotalBusinessSeats, int flightTotalBusinessPassengers) {
+        this.flightDepartureDate = flightDepartureDate;
+        this.flightDepartureTime = flightDepartureTime;
+        this.flightArrivalDate = flightArrivalDate;
+        this.flightArrivalTime = flightArrivalTime;
         this.flightPrice = flightPrice;
         this.flightTotalSeats = flightTotalSeats;
-        this.flightTotalPassengers = 0;
+        this.flightTotalPassengers = flightTotalPassengers;
+        this.flightBusinessPrice = flightBusinessPrice;
+        this.flightTotalBusinessSeats = flightTotalBusinessSeats;
+        this.flightTotalBusinessPassengers = flightTotalBusinessPassengers;
     }
 
-    // Getters and setters
-    public String getFlightDate() {
-        return flightDate;
+
+
+    public String getFlightDepartureDate() {
+        return flightDepartureDate;
     }
-    public String getFlightTime() {
-        return flightTime;
+
+
+
+    public void setFlightDepartureDate(String flightDepartureDate) {
+        this.flightDepartureDate = flightDepartureDate;
     }
+
+
+
+    public String getFlightDepartureTime() {
+        return flightDepartureTime;
+    }
+
+
+
+    public void setFlightDepartureTime(String flightDepartureTime) {
+        this.flightDepartureTime = flightDepartureTime;
+    }
+
+
+
+    public String getFlightArrivalDate() {
+        return flightArrivalDate;
+    }
+
+
+
+    public void setFlightArrivalDate(String flightArrivalDate) {
+        this.flightArrivalDate = flightArrivalDate;
+    }
+
+
+
+    public String getFlightArrivalTime() {
+        return flightArrivalTime;
+    }
+
+
+
+    public void setFlightArrivalTime(String flightArrivalTime) {
+        this.flightArrivalTime = flightArrivalTime;
+    }
+
+
+
     public double getFlightPrice() {
         return flightPrice;
     }
+
+
+
+    public void setFlightPrice(double flightPrice) {
+        this.flightPrice = flightPrice;
+    }
+
+
+
     public int getFlightTotalSeats() {
         return flightTotalSeats;
     }
+
+
+
+    public void setFlightTotalSeats(int flightTotalSeats) {
+        this.flightTotalSeats = flightTotalSeats;
+    }
+
+
+
     public int getFlightTotalPassengers() {
         return flightTotalPassengers;
     }
 
-    public void setFlightDate(String flightDate) {
-        this.flightDate = flightDate;
+
+
+    public void setFlightTotalPassengers(int flightTotalPassengers) {
+        this.flightTotalPassengers = flightTotalPassengers;
     }
-    public void setFlightTime(String flightTime) {
-        this.flightTime = flightTime;
+
+
+
+    public double getFlightBusinessPrice() {
+        return flightBusinessPrice;
     }
-    public void setFlightPrice(double flightPrice) {
-        this.flightPrice = flightPrice;
+
+
+
+    public void setFlightBusinessPrice(double flightBusinessPrice) {
+        this.flightBusinessPrice = flightBusinessPrice;
     }
-    public void setFlightTotalSeats(int flightTotalSeats) {
-        this.flightTotalSeats = flightTotalSeats;
+
+
+
+    public int getFlightTotalBusinessSeats() {
+        return flightTotalBusinessSeats;
     }
+
+
+
+    public void setFlightTotalBusinessSeats(int flightTotalBusinessSeats) {
+        this.flightTotalBusinessSeats = flightTotalBusinessSeats;
+    }
+
+
+
+    public int getFlightTotalBusinessPassengers() {
+        return flightTotalBusinessPassengers;
+    }
+
+
+
+    public void setFlightTotalBusinessPassengers(int flightTotalBusinessPassengers) {
+        this.flightTotalBusinessPassengers = flightTotalBusinessPassengers;
+    }
+
+    
 }
